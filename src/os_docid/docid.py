80953ecfdc0aa0cac8f28da33ec5fc85530447ea
cfhamlet <cfhamlet@gmail.com>
2018-01-29 15:13:38 +0800

    Update docid.py

--- src/os_docid/docid.py
+++ src/os_docid/docid.py
@@ -2,8 +2,8 @@ import binascii
 
 
 class DocID(object):
-    def __init__(self, b_domainid, siteid, b_urlid):
-        self._b_parts = (b_domainid, siteid, b_urlid)
+    def __init__(self, b_domainid, b_siteid, b_urlid):
+        self._b_parts = (b_domainid, b_siteid, b_urlid)
 
     def __str__(self):
         return '-'.join([binascii.hexlify(i) for i in self._b_parts])
