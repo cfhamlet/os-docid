ab8c900d129cc6304154c8f156372760e8e61bf3
root <cfhamlet@gmail.com>
2018-03-16 11:56:50 +0800

    support py3

--- setup.py
+++ setup.py
@@ -37,5 +37,7 @@ setup(
         'Natural Language :: English',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: Implementation :: CPython',
     ])
